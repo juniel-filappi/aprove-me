dbbf8e830713b154e388ce27939549d180d6a97c
Juniel Filappi <juniel123123@outlook.com>
2024-11-13 00:43:41 -0400

    feature: create user module

---
+++ back/src/user/user.repository.ts
@@ -0,0 +1,16 @@
+import { PrismaService } from "../prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
+import { RegisterDto } from "../shared/dto/auth/register.dto";
+
+@Injectable()
+export class UserRepository {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async findUnique(where: { email: string }) {
+        return this.prisma.user.findUnique({ where });
+    }
+
+    async create(data: RegisterDto) {
+        return this.prisma.user.create({ data });
+    }
+}
---
+++ back/src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { UserRepository } from "./user.repository";
+import { RegisterDto } from "../shared/dto/auth/register.dto";
+import { hash } from "bcryptjs";
+
+@Injectable()
+export class UserService {
+    constructor(private readonly repository: UserRepository) {}
+
+    async findUserByEmail(email: string) {
+        return this.repository.findUnique({ email });
+    }
+
+    async create(registerDto: RegisterDto) {
+        const user = await this.findUserByEmail(registerDto.email);
+
+        if (user) {
+            throw new HttpException("User already exists", HttpStatus.BAD_REQUEST);
+        }
+
+        const hashedPassword = await hash(registerDto.password, 8);
+
+        const dataToSave = {
+            ...registerDto,
+            password: hashedPassword
+        }
+
+        return this.repository.create(dataToSave);
+    }
+}
